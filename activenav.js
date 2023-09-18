06835969a49fa3a33175e038378826d303dabec0
Marius Odediran <diranmarius8@gmail.com>
2023-09-18 22:15:24 +0100

    Adding the next page

--- activenav.js
+++ activenav.js
@@ -1,10 +1,10 @@
 const link = document.querySelectorAll('a');
+const listItems = document.querySelectorAll('li');
 
 link.forEach(link =>{
-  link.addEventListener('click', function(event)  {
-    const listItems = document.querySelectorAll('li');
+  link.addEventListener('click', function(event) {
     listItems.forEach(item => {
-      if(item === event.target){
+      if(item === event.target.parentElement){
         item.style.borderBottom = '3px solid rgb(255, 255, 255)';
       } else{
         item.style.borderBottom = 'none';
