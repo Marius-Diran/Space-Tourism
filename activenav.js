7ba851462f94e92f08f6e55be645f52089770d34
Marius Odediran <diranmarius8@gmail.com>
2023-09-18 14:41:00 +0100

    Trying to add a JS code

--- activenav.js
+++ activenav.js
@@ -1,6 +1,14 @@
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('nav a').forEach(link => {
-  if(link.href.includes(`${activePage}`)){
-    link.classList.add('active');
-  }
-})
+const link = document.querySelectorAll('a');
+
+link.forEach(link =>{
+  link.addEventListener('click', function(event)  {
+    const listItems = document.querySelectorAll('li');
+    listItems.forEach(item => {
+      if(item === event.target){
+        item.style.borderBottom = '3px solid rgb(255, 255, 255)';
+      } else{
+        item.style.borderBottom = 'none';
+      }
+    });
+  });
+});
